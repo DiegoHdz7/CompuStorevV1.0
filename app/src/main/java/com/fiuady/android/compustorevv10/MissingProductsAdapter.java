988a3e8273756e87412e662847e43eefd4c6e749
DiegoHdz7 <diego.hdz.glez@gmail.com>
2017-04-21 20:56:25 -0500

    Initial Commit

--- app/src/main/java/com/fiuady/android/compustorevv10/MissingProductsAdapter.java
+++ app/src/main/java/com/fiuady/android/compustorevv10/MissingProductsAdapter.java
@@ -1,8 +1,24 @@
 package com.fiuady.android.compustorevv10;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
 /**
  * Created by Diego Hernandez on 21/04/2017.
  */
 
 public class MissingProductsAdapter {
+
+    public static class ProductViewHolder extends RecyclerView.ViewHolder /*implements View.OnClickListener*/ {
+
+        private TextView IDtxt, DesTxt, priceTxt, qtyTxt;
+
+        public ProductViewHolder(View itemView) {
+            super(itemView);
+
+
+
+        }
+    }
 }
